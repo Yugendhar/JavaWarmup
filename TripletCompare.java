a2c4dd1e569ca70ece278777eae679d6a1974eb2
Yugendhar <yogi.yugendhar@gmail.com>
2016-10-04 20:44:59 -0400

    Create TripletCompare.java
    input: 
    5 6 7
    3 6 10
    output
    1 1
    
    Alice 1 point
    Bob 1 point

---
+++ TripletCompare.java
@@ -0,0 +1,31 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int a0 = in.nextInt();
+        int a1 = in.nextInt();
+        int a2 = in.nextInt();
+        int b0 = in.nextInt();
+        int b1 = in.nextInt();
+        int b2 = in.nextInt();
+        int aScore,bScore=0;
+        
+        aScore = compareScores(a0,b0)+compareScores(a1,b1)+compareScores(a2,b2);
+        bScore = compareScores(b0,a0)+compareScores(b1,a1)+compareScores(b2,a2);
+        System.out.println(aScore+" "+bScore);
+       }
+        
+        static int compareScores(int a, int b)
+            {
+            if(a>b)
+            return 1;
+            else
+              return 0;
+        }     
+}
